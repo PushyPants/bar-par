4ae9d5e4d162ff73a1257f2402380fc05f4c2321
Josh Willems <jscottwillems@gmail.com>
2018-09-29 10:51:22 -0500

    added login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 
