f0e3d269a45bf7cbbf38fa48c41dea8395d146f1
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-10-06 15:01:42 -0500

    added markers on the time slider

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -2,23 +2,35 @@ import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import Login from "../../components/Login";
 import "./Landing.css";
-// import DatePickers from "../../components/DatePicker/DatePicker";
+import DatePickers from "../../components/DatePicker/DatePicker";
 import "rc-slider/assets/index.css";
 import "rc-tooltip/assets/bootstrap.css";
-// import MySlider from "../../components/Slider/Slider";
+import MySlider from "../../components/Slider/Slider";
+
+
 
 class Landing extends Component {
+  // state = {
+  //   Date: ""
+  // }
+  // handleChange(e) {
+  //   this.setState({
+  //     Date: e.target.vale
+  //   })
+  //   console.log(this.state.Date);
+  // }
   render() {
     return (
       <div>
         <Nav>Bar Par </Nav>
         <Login />
-        {/* <DatePickers>
-          
-        </DatePickers>
+        {/* <DatePickers
+          myChange={() => this.handleChange()}
+          value={this.state.Date}
+        /> */}
         <div>
           <MySlider />
-        </div> */}
+        </div>
       </div>
     );
   }
