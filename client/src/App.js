80688bf9d35d83d9eef9b0b530835e95ff5998f5
David Luu <David.n.luu@gmail.com>
2018-10-08 07:38:57 -0500

    Design change Landing to SignIn

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Employee from "./pages/Employee"
+import SignIn from "./pages/SignIn";
+import Employee from "./pages/Employee";
 import Availability from "./pages/Availability";
-import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import Locations from "./pages/Locations";
-
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const App = () => (
-    <Router>
-      <div>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/addemp" component={Employee} />
-            <Route exact path="/addavail" component={Availability} />
-            <Route exact path="/locations" component={Locations} />
-          </Switch>
-      </div>
-    </Router>
+  <Router>
+    <div>
+      <CssBaseline />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/addemp" component={Employee} />
+        <Route exact path="/addavail" component={Availability} />
+        <Route exact path="/locations" component={Locations} />
+      </Switch>
+    </div>
+  </Router>
 );
 
-export default App; 
+export default App;
