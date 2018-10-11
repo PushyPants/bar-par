ee243efad9c727dbe7fb3c8257a9744a95a71960
Josh Willems <jscottwillems@gmail.com>
2018-10-11 18:24:25 -0500

    preparing to merge

--- client/src/pages/Locations/Locations.js
+++ client/src/pages/Locations/Locations.js
@@ -6,11 +6,12 @@ import API from "../../utils/API"
 import Footer from "../../components/Footer";
 import LocationItem from "../../components/LocationItem";
 import "./Locations.css";
+import { ListItem } from "@material-ui/core";
+import { List } from "@material-ui/core";
 
 class Locations extends Component {
     state = {
         Locations: [],
-        name: "test"
     };
 
     componentDidMount = () => {
@@ -27,20 +28,41 @@ class Locations extends Component {
     }
 
     render() {
+        let list = this.state.Locations.map((location) => <LocationItem key={location.location_id} name={location.name}>
+                    <div className="location-preview">
+                        <div className="product-name">
+                            <List>
+                                <ListItem><strong>Product Name</strong></ListItem>
+                                <ListItem>Deep Eddy</ListItem> {/*need map for each product name*/}
+                            </List>
+                        </div>
+                        <div className="inv-date">
+                            <List>
+                                <ListItem><strong>Last Updated</strong></ListItem>
+                                <ListItem>Oct. 5, 2018</ListItem> {/*need map for each date*/}
+                            </List>
+                        </div>
+                        <div className="updated-by">
+                            <List>
+                                <ListItem><strong>Updated By</strong></ListItem>
+                                <ListItem>PushyPants</ListItem> {/*need map for each username name*/}
+                            </List>
+                        </div>
+                        <div className="quanty">
+                            <List>
+                                <ListItem><strong>Qty.</strong></ListItem>
+                                <ListItem>1345</ListItem> {/*need map for each qty*/}
+                            </List>
+                        </div>
+                    </div>
+        </LocationItem>)
+
         return (
         <React.Fragment>
             <Nav>Locations</Nav>
-            
             <div className="locations-list">
-                <LocationItem name={"Location passed name"}>This is the children</LocationItem>
-                <LocationItem />
-                <LocationItem />
-                <LocationItem />
-                <LocationItem />
-                <LocationItem />
-                <LocationItem />
+                {this.state.Locations.length > 0 ? list : null}
             </div>
-
             <Footer />
         </React.Fragment>
         );
