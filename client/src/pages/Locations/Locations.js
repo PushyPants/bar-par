5d36acdd5f5be8ae9d1d72f73a08de7b29928490
Josh Willems <jscottwillems@gmail.com>
2018-10-11 20:34:01 -0500

    push is waitingggg

--- client/src/pages/Locations/Locations.js
+++ client/src/pages/Locations/Locations.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Nav from "../../components/Nav";
-import { connect } from 'react-redux';
-import * as actions from '../../store/actions';
-import API from "../../utils/API"
+import { connect } from "react-redux";
+import * as actions from "../../store/actions";
+import API from "../../utils/API";
 import Footer from "../../components/Footer";
 import LocationItem from "../../components/LocationItem";
 import "./Locations.css";
@@ -10,63 +10,85 @@ import { ListItem, Divider } from "@material-ui/core";
 import { List } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
+const style = {
+  stationLink: {
+    width: "80%",
+    margin: "auto"
+  }
+};
+
 class Locations extends Component {
-    state = {
-        Locations: [],
-    };
+  state = {
+    Locations: []
+  };
 
-    componentDidMount = () => {
-        this.getLocations()
-    };
+  componentDidMount = () => {
+    this.getLocations();
+  };
 
-    getLocations = () => {
-        API.getLocations().then(res => {
-            console.log(res.data);
-            this.setState({ Locations: res.data });
-        });
-        // console.log("[getLocations] results after await: ",results);
+  getLocations = () => {
+    API.getLocations().then(res => {
+      console.log(res.data);
+      this.setState({
+        Locations: res.data
+      });
+    });
+    // console.log("[getLocations] results after await: ",results);
+  };
 
-    }
-
-    render() {
-
-        return (
-        <React.Fragment>
-            <Nav>Locations</Nav>
-            <div className="locations-list">
-                <LocationItem>
-                            <List>
-                                <ListItem><Button className="station-link"><strong>Speed Rail 01</strong></Button></ListItem>
-                                <Divider/>
-                                <ListItem className="station-link"><Button ><strong>Speed Rail 02</strong></Button></ListItem>
-                                <Divider/>
-                                <ListItem><Button className="station-link"><strong>Speed Rail 03</strong></Button></ListItem>
-                                <Divider/>
-                                <ListItem><Button className="station-link"><strong>Speed Rail 04</strong></Button></ListItem>
-                                <Divider/>
-                                <ListItem><Button className="station-link"><strong>Speed Rail 05</strong></Button></ListItem>
-                                <Divider/>
-                                <ListItem><Button className="station-link"><strong>Speed Rail 06</strong></Button></ListItem>
-                                <Divider/>
-                            </List>
-                </LocationItem>
-            </div>
-            <Footer />
-        </React.Fragment>
-        );
-    }
+  render() {
+    const classes = this.props;
+    return (
+      <React.Fragment>
+        <Nav> Locations </Nav>
+        <div className="locations-list">
+          <LocationItem>
+            <List>
+              <ListItem>
+                <Button className={classes.stationLink}> Speed Rail 01 </Button>
+              </ListItem>
+              <Divider />
+              <ListItem className="station-link">
+                <Button> Speed Rail 02 </Button>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <Button className="station-link"> Speed Rail 03 </Button>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <Button className="station-link"> Speed Rail 04 </Button>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <Button className="station-link"> Speed Rail 05 </Button>
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <Button className="station-link"> Speed Rail 06 </Button>
+              </ListItem>
+              <Divider />
+            </List>
+          </LocationItem>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     employeeList: state.reducer.employeeList
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getEmployeeList: () => dispatch(actions.getEmployeeList()),
-  }
-}
+    getEmployeeList: () => dispatch(actions.getEmployeeList())
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Locations);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Locations);
