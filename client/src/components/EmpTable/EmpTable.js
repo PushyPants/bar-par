53a87cea4e9d8969beca9545c5fc0823c4c92afd
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-10-12 02:44:00 -0500

    changed locations component and locationitem component with redux to display dropdowns

--- client/src/components/EmpTable/EmpTable.js
+++ client/src/components/EmpTable/EmpTable.js
@@ -1,11 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-// import Table from "@material-ui/core/Table";
-// import TableBody from "@material-ui/core/TableBody";
-// import TableCell from "@material-ui/core/TableCell";
-// import TableHead from "@material-ui/core/TableHead";
-// import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
@@ -14,7 +9,7 @@ import Grid from "@material-ui/core/Grid";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Typography from "@material-ui/core/Typography";
 import DeleteBtn from "../DeleteBtn";
-// import API from "../../utils/API";
+import Icon from '@material-ui/core/Icon';
 
 const styles = theme => ({
   root: {
@@ -29,7 +24,7 @@ const styles = theme => ({
 });
 
 const divStyle = {
-  padding: '0px'
+  padding: "0px"
 };
 
 let id = 0;
@@ -38,56 +33,42 @@ function createData(name, phone, email, empId) {
   return { id, name, phone, email, empId };
 }
 
-
 function EmpTable(props) {
   const { classes } = props;
   const rows = [];
-  
-  const delEmp = (empId) => {
-    props.deleteEmployee(empId)
-  }
+
+  const delEmp = empId => {
+    props.deleteEmployee(empId);
+  };
 
   props.empArr.map(emp =>
     rows.push(
-      createData(`${emp.firstName} ${emp.lastName}`, emp.phone, emp.email, emp._id)
+      createData(
+        `${emp.firstName} ${emp.lastName}`,
+        emp.phone,
+        emp.email,
+        emp._id
+      )
     )
   );
 
   return (
     <Paper className={classes.root}>
-      {/* <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.root}>Name</TableCell>
-            <TableCell className={classes.root}>Phone</TableCell>
-            <TableCell className={classes.root}>Email</TableCell>
-            <TableCell className={classes.root}></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody> */}
       {rows.map(row => {
         return (
-          // <TableRow key={row.id}>
-          //   <TableCell className={classes.root} component="th" scope="row">
-          //     {row.name}
-          //   </TableCell>
-          //   <TableCell className={classes.root} numeric>
-          //     {row.phone}
-          //   </TableCell>
-          //   <TableCell className={classes.root} numeric>
-          //     {row.email}
-          //   </TableCell>
           <ExpansionPanel style={{ width: "100%" }} key={row.id}>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
               {row.name}
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Grid item xs={9} sm={8}>
-                <Typography style={divStyle} align={'left'}>
-                  Phone: {row.phone} <br/>Email: {row.email}
+                <Typography style={divStyle} align={"left"}>
+                  Phone: {row.phone} <br />
+                  Email: {row.email}
                 </Typography>
               </Grid>
               <Grid item xs={3} sm={4}>
+
                 <DeleteBtn
                   valOne={row.empId}
                   valTwo={null}
@@ -96,15 +77,20 @@ function EmpTable(props) {
                 >
                   <i className="material-icons">delete_outline</i>
                 </DeleteBtn>
+                <DeleteBtn
+                  valOne={row.empId}
+                  valTwo={null}
+                  // func={}
+                  color="primary"
+                  aria-label="Edit"
+                >
+                  <Icon>edit_icon</Icon>
+                </DeleteBtn>
               </Grid>
             </ExpansionPanelDetails>
           </ExpansionPanel>
-
-          // </TableRow>
         );
       })}
-      {/* </TableBody>
-      </Table> */}
     </Paper>
   );
 }
--- client/src/pages/Locations/Locations.js
+++ client/src/pages/Locations/Locations.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions";
-import API from "../../utils/API";
-import Footer from "../../components/Footer";
+// import API from "../../utils/API";
+// import Footer from "../../components/Footer";
 import LocationItem from "../../components/LocationItem";
 import "./Locations.css";
-import { ListItem, Divider } from "@material-ui/core";
-import { List } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
+// import { ListItem, Divider } from "@material-ui/core";
+// import { List } from "@material-ui/core";
+// import Button from "@material-ui/core/Button";
+// import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+// import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+// import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const style = {
   stationLink: {
@@ -19,57 +23,28 @@ const style = {
 
 class Locations extends Component {
   state = {
-    Locations: []
+    location_id: "",
+    name: "",
+    parent_location: "",
+    positions: []
   };
 
-  componentDidMount = () => {
+  componentWillMount() {
     this.getLocations();
-  };
+  }
 
   getLocations = () => {
-    API.getLocations().then(res => {
-      console.log(res.data);
-      this.setState({
-        Locations: res.data
-      });
-    });
-    // console.log("[getLocations] results after await: ",results);
+     this.props.getLocation();
   };
 
   render() {
-    const classes = this.props;
+    // const { classes } = this.props;
+
     return (
       <React.Fragment>
         <Nav> Locations </Nav>
         <div className="locations-list">
-          <LocationItem>
-            <List>
-              <ListItem>
-                <Button className={classes.stationLink}> Speed Rail 01 </Button>
-              </ListItem>
-              <Divider />
-              <ListItem className="station-link">
-                <Button> Speed Rail 02 </Button>
-              </ListItem>
-              <Divider />
-              <ListItem>
-                <Button className="station-link"> Speed Rail 03 </Button>
-              </ListItem>
-              <Divider />
-              <ListItem>
-                <Button className="station-link"> Speed Rail 04 </Button>
-              </ListItem>
-              <Divider />
-              <ListItem>
-                <Button className="station-link"> Speed Rail 05 </Button>
-              </ListItem>
-              <Divider />
-              <ListItem>
-                <Button className="station-link"> Speed Rail 06 </Button>
-              </ListItem>
-              <Divider />
-            </List>
-          </LocationItem>
+          <LocationItem locArr={this.props.Locations} />
         </div>
       </React.Fragment>
     );
@@ -78,13 +53,13 @@ class Locations extends Component {
 
 const mapStateToProps = state => {
   return {
-    employeeList: state.reducer.employeeList
+    Locations: state.reducer.Locations
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    getEmployeeList: () => dispatch(actions.getEmployeeList())
+    getLocation: () => dispatch(actions.getLocation())
   };
 };
 
